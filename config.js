c6fb7720ba6072c7c89974f1c23cd4cb7e27b9b0
ASYRAFBOTZ <102161935+ASYRAFBOTZ@users.noreply.github.com>
2022-07-24 19:01:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6281351047727'] // Put your number here
+global.owner = ['6281946945315'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -28,7 +28,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'Mark Botz'
+global.packname = 'Azura Botz'
 global.author = 'By Asyraf'
 global.email = 'wekasenpai@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
