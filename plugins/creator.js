5f68581a7147d716682256d7278f6eecd4454583
ASYRAFBOTZ <102161935+ASYRAFBOTZ@users.noreply.github.com>
2022-07-24 15:41:49 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,16 +2,16 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Ilman;;;'
-                    + 'FN:Ilman\n' // full name
-                    + 'ORG:Ilman;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281351047727:+62 813-5104-7727\n' // WhatsApp ID + phone number
+                    + 'N:;YT : MikAzu 929;;;'
+                    + 'FN:YT : MikAzu 929\n' // full name
+                    + 'ORG:Asyraf;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6281946945315:+62 819-4694-5315\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Ilman', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: '', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator)$/i
+handler.command = /^(owner|creator|oraf)$/i
 
 module.exports = handler
