4a11db8dcf7c5d938e081ce8dc12d119a8bb6f56
ASYRAFBOTZ <102161935+ASYRAFBOTZ@users.noreply.github.com>
2022-07-24 19:42:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.author = 'By Asyraf'
 global.email = 'wekasenpai@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.wm = '*YT : MikAzu 929*'
-global.media = ''https://telegra.ph/file/6c740ac2a0f2b15267176.jpg'
+global.media = 'https://telegra.ph/file/6c740ac2a0f2b15267176.jpg'
 
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
@@ -53,12 +53,14 @@ global.rpg = {
       mythic: '🗳️',
       legendary: '🗃️',
       pet: '🎁',
+      anvered: '🔮',
       sampah: '🗑',
       armor: '🥼',
       sword: '⚔️',
       kayu: '🪵',
       batu: '🪨',
       string: '🕸️',
+      ratumedusa: '🐍',
       kuda: '🐎',
       kucing: '🐈' ,
       anjing: '🐕',
